1692bcd842eff744f82ebb20b6fa32943f14adaa
Abhishek Kumar Singh <abhishek10k8singh@gmail.com>
2025-08-22 02:24:16 +0530

    C05: AI jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,20 @@
 import { inngest } from "./client";
+import { openai, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const summarizer = createAgent({
+      name: "summarizer",
+      system: "You are an expert summarizer.  You summarize in 2 words.",
+      model: openai({ model: "gpt-4o-mini" }),
+    });
+
+    const { output } = await summarizer.run(
+      `Summarize the following text: ${event.data.value}`,
+    );
+    console.log(output);
+    return { output };
   },
 );
