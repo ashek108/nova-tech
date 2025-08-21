74841cd9ce5376dfc90fe4d81e02ce3fc7897bd0
Abhishek Kumar Singh <abhishek10k8singh@gmail.com>
2025-08-21 16:30:45 +0530

    C03: trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import { useTRPC } from '@/trpc/client';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 
 const Page = () => {
+  const trpc = useTRPC();
+  // const { data } = useQuery()
   return (
     <div>
       Hello World!
