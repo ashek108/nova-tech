d31a87d935019f3ad1a8753aec0bc06244cd2239
Abhishek Kumar Singh <abhishek10k8singh@gmail.com>
2025-08-22 17:52:26 +0530

    C06: E2B Sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
+import {Sandbox} from "@e2b/code-interpreter";
 import { openai, createAgent } from "@inngest/agent-kit";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("nova-nextjs-ashek-test");
+      return sandbox.sandboxId;
+    });
+
     const summarizer = createAgent({
       name: "summarizer",
       system: "You are an expert summarizer.  You summarize in 2 words.",
@@ -14,7 +21,13 @@ export const helloWorld = inngest.createFunction(
     const { output } = await summarizer.run(
       `Summarize the following text: ${event.data.value}`,
     );
-    console.log(output);
-    return { output };
+    
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `http://${host}`;
+    });
+
+    return { output, sandboxUrl };
   },
 );
