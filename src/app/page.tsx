7cbb13658736594814b15edc5b7da35a6b04abd1
Abhishek Kumar Singh <abhishek10k8singh@gmail.com>
2025-08-24 14:15:57 +0530

    08: messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
+
 const Page = () => {
+  const [value, setValue] = useState("");
+  const trpc = useTRPC();
+  const {data: messages} = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessage = useMutation(trpc.messages.create.mutationOptions({
+    onSuccess: () => {
+      toast.success("Message created successfully!");
+    }
+  }));
+
   return (
     <div>
-      Hello World!
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button
+        disabled={createMessage.isPending} onClick={() => createMessage.mutate({value: value})}
+      >
+        Invoke Background Job 
+      </Button>
+      {JSON.stringify(messages, null, 2)}
     </div>
   )
 }
