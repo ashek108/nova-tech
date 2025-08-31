c2e5733a1c45dc814a5f50fea430f86a64c344b1
Abhishek Kumar Singh <abhishek10k8singh@gmail.com>
2025-08-31 16:23:23 +0530

    C26: deploy 6

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,20 @@ const nextConfig: NextConfig = {
     // ESLint configuration options
     ignoreDuringBuilds: true,
   },
+
+   async headers() {
+    return [
+      {
+        source: "/(.*)", // all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://nova-tech-psi.vercel.app https://*.e2b.dev;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
